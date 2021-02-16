a424b79e1f0ea717b30d1fb1624a92059bb0fc77
e <2312187195@qq.com>
2021-02-16 12:49:21 +0800

    商品详情页-加入购物车的数据进行本地化存储

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -18,12 +18,40 @@ Page({
     //加载框
     await showLoading()
     let res = await detail(id)
-    console.log(res)
+    // console.log(res)
 
     this.setData({
       detailData: res
     })
 
     await hideLoading()
+  },
+
+  //加入购物车
+  addCart(){
+    let {goods_id,goods_name, goods_price, goods_small_logo } = this.data.detailData
+    //1、本地获取数据（购物车），无：空数组
+    //2、判断购物车中有无当前点击的商品id
+    //3、有：商品数量累加；无：将商品组成对象添加到购物车数组中
+    //4、购物车保存在本地
+
+    let cart = wx.getStorageSync('cart') || [];
+    let goods = cart.find(item=>item.goods_id===goods_id)
+
+    if(goods){
+      //购物车中有商品
+      goods.goods_num++
+    }else {
+      //无商品: 放入购物车中
+      cart.push({
+        goods_id,
+        goods_name,
+        goods_price,
+        goods_small_logo,
+        goods_num: 1
+      })
+    }
+
+    wx.setStorageSync('cart', cart); //购物车本地保存
   }
 })
